d4f5c423d3d0391a6508bb3638e58338bda38930
Carter Stackhouse <94951260+CarterS125@users.noreply.github.com>
2022-10-26 21:52:02 -0600

    Add files via upload

---
+++ google-2.0-nextjs-tailwind-main/keys.js
@@ -0,0 +1,5 @@
+// https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "<REPLACE WITH YOUR KEY>";
+
+// https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "9867c733a42c0c643"; // << Replace with your own CX key
